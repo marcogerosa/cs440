ee3f30cad69e2a65648525a3b7a3ddd7ada761af
Marco Aurelio Gerosa <profgerosa@gmail.com>
2024-11-02 17:48:24 -0700

    Create A43_LSP_Java.java

---
+++ A43_LSP_Java.java
@@ -0,0 +1,28 @@
+class FileInputStream {
+   File file;
+   int position = 0;
+   public int read() {
+       if (file.endOfFile() == true) {
+           return -1;
+       } else {
+           position = position + 1;
+           readNumber(position);
+       }
+   }
+   int readNumber(int position) {
+       // some complicated logic
+   }
+}
+
+
+class EndlessFileInputStream extends FileInputStream
+{
+   public int read() {
+       if (file.endOfFile() == true) {
+           position = 0;
+       } else {
+           position = position + 1;
+           return readNumber(position);
+       }
+   }
+}
