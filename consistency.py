e938ea6de99a793d9701154160b4185f112d2d60
Marco Aurelio Gerosa <profgerosa@gmail.com>
2025-03-11 15:55:46 -0700

    Create consistency.py

---
+++ consistency.py
@@ -0,0 +1,32 @@
+class UserManager:
+    def create_new_user(self, username, password):
+        pass
+    
+    def getExistingUserById(self, user_id):
+        pass
+    
+    def modify_user_data(self, userData):
+        pass
+    
+    def DeleteUserAccount(self, userId):
+        pass
+
+class OrderManager:
+    def createOrder(self, order_details):
+        pass
+    
+    def get_order(self, orderId):
+        pass
+    
+    def UPDATE_ORDER_STATUS(self, order):
+        pass
+
+class ProductHandler:
+    def addNewProduct(self, product_info):
+        pass
+    
+    def find_product_by_id(self, id):
+        pass
+    
+    def removeProduct(self, productId):
+        pass
